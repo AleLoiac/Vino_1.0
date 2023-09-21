ccf9767ae80326568cf732882965271e2bf768ff
AleLoiac <alessandroloiac@gmail.com>
2023-09-21 17:57:20 +0200

    21/09

--- database/databasetup.go
+++ database/databasetup.go
@@ -1,5 +1,50 @@
 package database
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
 
+func DBSet() *mongo.Client {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cancel()
+
+	err = client.Disconnect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb :(")
+		return nil
+	}
+
+	fmt.Println("Successfully connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
 }
